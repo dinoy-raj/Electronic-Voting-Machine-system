e9b2e80f633079d5a0c79f1b8f089eaad75c516f
Amal Nath M <amalnathm7@gmail.com>
2022-02-27 16:33:42 +0530

    SQL Queries

--- src/database/MySQL.java
+++ src/database/MySQL.java
@@ -1,6 +1,7 @@
 package database;
 
 import java.sql.*;
+import java.util.HashMap;
 
 public class MySQL {
     String username = "root";
@@ -8,7 +9,6 @@ public class MySQL {
     String dbName = "VotR";
     String url = "jdbc:mysql://localhost:3306/";
     Connection connection;
-    private ResultSet rs;
 
     public void connectJDBC() {
         try {
@@ -18,24 +18,20 @@ public class MySQL {
         }
     }
 
-    public void retrieveData(String table) {
+    public void addUser(String ID, boolean isAdmin) {
         try {
             Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM " + table + ";");
-
-            while (rs.next()) {
-                System.out.print("ID: " + rs.getInt("id"));
-                System.out.println(", Name: " + rs.getString("name"));
-            }
+            stmt.executeUpdate("INSERT INTO USERS VALUES('" + ID + "', " + isAdmin + ");");
         } catch (SQLException e) {
             e.printStackTrace();
+
         }
     }
 
-    public void addElection(String ID, String name, boolean isStarted) {
+    public void addElection(String ID, String creatorID, String name, boolean isStarted, boolean isEnded) {
         try {
             Statement stmt = connection.createStatement();
-            stmt.executeUpdate("INSERT INTO ELECTIONS VALUES('" + ID + "', " + "'" + name + "', " + isStarted + ");");
+            stmt.executeUpdate("INSERT INTO ELECTIONS VALUES('" + ID + "', '" + creatorID + "', '" + name + "', " + isStarted + ", " + isEnded + ");");
         } catch (SQLException e) {
             e.printStackTrace();
 
@@ -92,7 +88,99 @@ public class MySQL {
         }
     }
 
-    public void test() {
 
+    public ResultSet retrieveData(String table) {
+        try {
+            Statement stmt = connection.createStatement();
+            return stmt.executeQuery("SELECT * FROM " + table + ";");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public ResultSet getOngoingElections(String userID) {
+        try {
+            Statement stmt = connection.createStatement();
+            return stmt.executeQuery("SELECT * FROM ELECTIONS WHERE creator_id = '" + userID + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public ResultSet getEligibleElections(String voterID) {
+        try {
+            Statement stmt = connection.createStatement();
+            return stmt.executeQuery("SELECT * FROM ELIGIBLE_VOTERS WHERE voter_id = '" + voterID + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public HashMap<String, Object> startVote(String voterID, String password) {
+        try {
+            HashMap<String, Object> map = new HashMap<>();
+
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ELECTIONS JOIN ELIGIBLE_VOTERS ON id = election_id WHERE voter_id = '" + voterID + "' AND password = '" + password + "';");
+            rs.next();
+
+            if (!rs.getBoolean("isStarted")) {
+                HashMap<String, Object> temp = new HashMap<>();
+
+                temp.put("error", "isNotStarted");
+
+                return temp;
+            }
+
+            String electionName = rs.getString("name");
+
+            Statement stmt1 = connection.createStatement();
+            ResultSet rs1 = stmt1.executeQuery("SELECT * FROM ELECTIONS E JOIN POSTS P ON E.id = P.election_id WHERE E.id = '" + rs.getString("id") + "'");
+
+            while (rs1.next()) {
+                String postName = rs1.getString("P.name");
+
+                Statement stmt2 = connection.createStatement();
+                ResultSet rs2 = stmt2.executeQuery("SELECT * FROM POSTS P JOIN CANDIDATES C ON P.id = C.post_id WHERE P.id = '" + rs1.getString("P.id") + "'");
+
+                HashMap<String, Object> map1 = new HashMap<>();
+
+                for (int i = 0; rs2.next(); i++) {
+                    Statement stmt3 = connection.createStatement();
+                    ResultSet rs3 = stmt3.executeQuery("SELECT * FROM VOTERS V JOIN CANDIDATES C ON V.id = C.voter_id WHERE V.id = '" + rs2.getString("C.voter_id") + "'");
+
+                    rs3.next();
+
+                    HashMap<String, String> map2 = new HashMap<>();
+                    map2.put("name", rs3.getString("V.name"));
+                    map2.put("id", rs3.getString("V.id"));
+
+                    map1.put(postName + i, map2);
+                }
+
+                map.put(electionName, map1);
+            }
+
+            return map;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void castVote(String ID) {
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("UPDATE CANDIDATES SET count = count + 1 WHERE id = '" + ID + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
