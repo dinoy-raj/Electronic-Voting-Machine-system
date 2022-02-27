666b7a5da1f5a587e7f024180e0fc347e16debbd
Amal Nath M <amalnathm7@gmail.com>
2022-02-27 07:54:24 +0530

    DB

--- src/database/MySQL.java
+++ src/database/MySQL.java
@@ -8,6 +8,7 @@ public class MySQL {
     String dbName = "VotR";
     String url = "jdbc:mysql://localhost:3306/";
     Connection connection;
+    private ResultSet rs;
 
     public void connectJDBC() {
         try {
@@ -31,10 +32,60 @@ public class MySQL {
         }
     }
 
-    public void insertIntoElection(int ID, String name) {
+    public void addElection(String ID, String name, boolean isStarted) {
         try {
             Statement stmt = connection.createStatement();
-            stmt.executeUpdate("INSERT INTO ELECTION VALUES(" + ID + ", " + "'" + name + "'" + ");");
+            stmt.executeUpdate("INSERT INTO ELECTIONS VALUES('" + ID + "', " + "'" + name + "', " + isStarted + ");");
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void addPost(String ID, String election_id, String name) {
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("INSERT INTO POSTS VALUES('" + ID + "', " + "'" + election_id + "', '" + name + "');");
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void addCandidate(String ID, String post_id, String voter_id, int count) {
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("INSERT INTO CANDIDATES VALUES('" + ID + "', '" + post_id + "', '" + voter_id + "', " + count + ");");
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void addVoter(String ID, String name, String gender, String address, String pinCode, String phone) {
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("INSERT INTO VOTERS VALUES('" + ID + "', " + "'" + name + "', '" + gender + "', '" + address + "',  '" + pinCode + "', '" + phone + "');");
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void addResult(String electionID, String winnerID) {
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("INSERT INTO RESULTS VALUES('" + electionID + "', '" + winnerID + "');");
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void addEligibleVoter(String electionID, String voterID, String password) {
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("INSERT INTO ELIGIBLE_VOTERS VALUES('" + electionID + "', " + "'" + voterID + "', '" + password + "');");
         } catch (SQLException e) {
             e.printStackTrace();
 
@@ -42,5 +93,6 @@ public class MySQL {
     }
 
     public void test() {
+
     }
 }
